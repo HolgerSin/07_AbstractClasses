629ef9231641d3d59acb54af71973d72ff50d047
Holger_CCB_VSC <86824184+HolgerSin@users.noreply.github.com>
2021-08-31 13:08:12 +0200

    all shapes

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -1,11 +1,35 @@
 package com.cc.java;
+
+import java.lang.Thread.State;
+
 public class App {
     public static void main(String[] args) {
       // to do
-    
+      Rectangle rectangle = new Rectangle(10, 10);
+      Circle circle = new Circle(5);
+      Circle circle2 = new Circle(1);
+      Triangle triangle = new Triangle(10, 5);
+
+
+      ausgabe("Fläche Rechteck: " + rectangle.area());
+
+      ausgabe("Fläche Kreis: " + circle.area());
+      ausgabe("Fläche Kreis2: " + circle2.area());
+      
+      ausgabe("Fläche Dreieck: " + triangle.area());
+      
+      ausgabe("Diff. r-t: " + areaDiff(rectangle, triangle));
+      ausgabe("Diff. r-c: " + areaDiff(rectangle, circle));
+      ausgabe("Diff. c-t: " + areaDiff(circle, triangle));
+
 
     }
+    
+    
 
+    static double areaDiff(Shape s1, Shape s2){
+      return s1.area() - s2.area();
+    }
     private static void ausgabe(String outStr) {
         System.out.println(outStr);
     }
